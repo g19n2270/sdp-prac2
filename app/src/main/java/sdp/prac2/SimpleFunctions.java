53e36c5b4da12ddfe84ccf8a3363513ff4dae0fe
Gosego <gosegomenwe@gmail.com>
2022-07-21 15:00:06 +0200

    Task3_by_g20k2504

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -4,4 +4,22 @@ import java.util.*;
 
 public class SimpleFunctions {
     public SimpleFunctions() {}
+    
+    public Task3(String s) {
+        
+        int count=0;
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(s.charAt(i) == '(')
+            {
+                count++;
+            }else {
+            count --;
+            }
+            //number of left bracket is greater than right
+            if(count<0) return false;
+        }
+        //check all opening bracket have a corresponding closing bracket
+        return count==0;
+            }
 }
