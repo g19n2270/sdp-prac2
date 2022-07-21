e348910586a7d5943b85ba7ef1a916f67ee598bd
Mavhungu1203 <mavhungu.monjelele@gmail.com>
2022-07-21 16:27:19 +0200

    Task5 added

--- app/src/main/java/sdp/prac2/SimpleFunctions.java
+++ app/src/main/java/sdp/prac2/SimpleFunctions.java
@@ -3,5 +3,27 @@ package sdp.prac2;
 import java.util.*;
 
 public class SimpleFunctions {
-    public SimpleFunctions() {}
+    public SimpleFunctions() {
+    }
+    public static boolean task5(List<Integer> perfect){
+        List<Integer> num2 = new ArrayList<Integer>();
+        int length = perfect.size();
+        for (int i= 0; i<length; i++ ){
+            num2.add(perfect.get(i));
+
+        }
+        Collections.sort(perfect);
+        for (int i=0; i < length; i++){
+            if (perfect.get(i) != num2.get(i)){
+                return false;
+            }
+            else{
+                return true;
+            }
+            
+        }
+
+    }
+
 }
+
